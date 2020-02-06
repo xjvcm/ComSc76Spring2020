bdad740a7df540f2f6d5a13655054624389c19e5
Yanni App13 <jonvincruman@gmail.com>
2020-02-06 13:34:20 -0800

    updated assignment 2 with more content

--- Assignments/File_Input_Output/manzanoJonathanAssignment2.java
+++ Assignments/File_Input_Output/manzanoJonathanAssignment2.java
@@ -11,22 +11,38 @@
 import java.io.*;
 
 public class manzanoJonathanAssignment2 {
-  public static void main(String[] args) throws Exception {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws IOException {
+    // Hello World
+    // System.out.pri*ntln("Hello world!");
+
+    try (
+      // Create Outputstream to the file
+      BitOutputStream output =
+          new BitOutputStream(new FileOutputStream("temp.dat"));
+    ) {
+      // for (int i = 1; 1 <= 10; i++) {
+      //   output.write(i);
+
+    }
   }
 }
 
-class BitOutputStream implements OutputStream {
+class BitOutputStream {
   // Add attributes
-  private byte bite;
+  protected FileOutputStream file;
+  protected byte bite = 00000000;
+  protected int count = 0;
 
   // Creates a BitOutputStream to write bits to the file.
-  BitOutputStream(File file) {
-
+  BitOutputStream(FileOutputStream file) {
+    this.file = file;
   }
 
   // Writes a bit '0' or '1' to the output stream.
   public void writeBit(char bit) {
+    bite += (byte)bit;
+    bite = bite << 1;
+    count += 1;
   }
 
   // Write a string of bits to the ouput stream.
