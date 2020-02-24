a252f7cabd0fc735c82bfa50ae320de5c11882e6
Yanni App13 <jonvincruman@gmail.com>
2020-02-24 13:50:13 -0800

    started list stacks and queues assignment

---
+++ Assignments/List_Stacks_Queues/manzanoJonathanAssignment5.java
@@ -0,0 +1,21 @@
+/*
+ * Programmer: Jonathan Manzano
+ *
+ * Date: 10 March 2020
+ *
+ * Assignment: List, Stacks, and Queues
+ *
+ * Description: Define a class named Point with tow data fields x and y to
+ * represent a point's x- and y-coordinates. Implement the Comparable interface
+ * for the comparing the points on x-coordinates. If two points have the same
+ * x-coordinates, compare their y-coordinates.
+ * 
+ * Define a class named CompareY that implements Comparator<Point>. Implement
+ * the compare method to compare two points on their y-coordinates. If two
+ * points have the same y-coordinates, compare their x-coordinates.
+ * 
+ * Randomly create 100 points and apply the Arrays.sort method to display the
+ * points in increasing order of their x-coordinates, and increasing order of
+ * their y-coordinates, respectively.
+ *
+ */
