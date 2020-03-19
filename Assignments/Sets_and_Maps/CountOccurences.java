5e22f5ed2380be6758f4d01ef03caacab5fce18d
Yanni App13 <jonvincruman@gmail.com>
2020-03-19 13:48:42 -0700

    refactor code

--- Assignments/Sets_and_Maps/CountOccurences.java
+++ Assignments/Sets_and_Maps/CountOccurences.java
@@ -11,37 +11,37 @@
  */
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 
-public class manzanoJonathanAssignment6SAM {
+public class CountOccurences {
   public static void main(String[] args) {
+    
     // Check command-line argument length
     if (args.length != 1) {
-      System.out.println("Usage: java manzanoJonathanAssignment6SAM " +
-          "textFileName");
+      System.out.println("Usage: java CountOccurences " +
+          "Text File Name");
       System.exit(1);
     }
-
-    // Instantiate file object to hold file passed in argument
-    File file = new File(args[0]);
-
-    TreeMap<String, Integer> treeMap = new TreeMap<>();
-
+    
     try {
+      // Instantiate file object to hold file passed in argument
+      File file = new File(args[0]);
+
       Scanner input = new Scanner(file);
 
+      TreeMap<String, Integer> treeMap = new TreeMap<>();
       while (input.hasNext()) {
         String line = input.nextLine();
-        String[] words = line.split("[ @!~{}\\[\\]$#^&*\n\t\r.,;?'\")(]");
-
+        String[] words = line.split("[\\s\\p{P}]+");
+  
         for (int i = 0; i < words.length; i++) {
-          if (words[i].trim().length() > 0 && words[i].trim().matches("[A-Z|a-z]+")) {
+          if (words[i].length() > 0 && words[i].matches("[\\w]+")) {
             String key = words[i].toLowerCase();
-
+  
             if (treeMap.get(key) != null) {
               int count = treeMap.get(key);
               count++;
@@ -52,17 +52,20 @@ public class manzanoJonathanAssignment6SAM {
           }
         }
       }
-    } catch (Exception ex) {
-      ex.printStackTrace();
+      Set<Map.Entry<String, Integer>> entrySet = treeMap.entrySet();
+  
+      // Retrieve an entry set for the tree map
+      System.out.println("\nDisplay words and their count in " +
+          " ascending order of the word");
+  
+      // Display words in aplhabetical order
+      for (Map.Entry<String, Integer> entry: entrySet) {
+        System.out.println(entry.getValue() + "\t" + entry.getKey());
+      }
+    } catch (FileNotFoundException ex) {
+      System.out.println("File does not exist!");
+      System.exit(1);
     }
 
-    Set<Map.Entry<String, Integer>> entrySet = treeMap.entrySet();
-
-    System.out.println("\nDisplay words and their count in " +
-        " ascending order of the wors");
-
-    for (Map.Entry<String, Integer> entry: entrySet) {
-      System.out.println(entry.getValue() + "\t" + entry.getKey());
-    }
   }
 }
